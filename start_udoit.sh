859e37c106010abf2c3d13ee7b8a919bf94005ef
panbed <contact@bedp.dev>
2025-12-11 15:02:44 -0500

    change start udoit script

--- start_udoit.sh
+++ start_udoit.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 cd /home/ubuntu/udoit
-docker compose up -d
+
+# check .env file for SETUP_COMPLETE
+if grep -q "SETUP_COMPLETE=true" .env; then
+    echo "Starting UDOIT..."
+    docker compose -f docker-compose.yml up -d
+else
+    echo "Setup is not complete, starting UCONFIGIT..."
+    docker compose -f docker-compose-setup.yml up -d
+fi
