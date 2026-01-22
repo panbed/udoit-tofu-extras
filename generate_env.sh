86f0e359163bca295a612bf940ac4173ad2eedf3
panbed <contact@bedp.dev>
2026-01-22 14:12:32 -0500

    using curl to get ec2 metadata as test

--- generate_env.sh
+++ generate_env.sh
@@ -3,5 +3,7 @@
 cd /home/ubuntu/udoit
 
 # get secrets json from secrets manager, parse using jq then dump all values into .env file
-aws secretsmanager get-secret-value --secret-id "UDOIT_Secrets" --query "SecretString" --output text | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' >> .env
+REGION=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+
+aws secretsmanager get-secret-value --region $REGION --secret-id "UDOIT_Secrets" --query "SecretString" --output text | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' >> .env
 echo "Appended secrets to .env file."
